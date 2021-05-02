947579b7b883ae1065ca17b67b33b8003c41aad1
toshiki-o23 <tsk.okmt.15@gmail.com>
2021-05-02 18:42:42 +0900

    [Update] app.vue

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -5,6 +5,11 @@
         {{ memo.title }}： {{ memo.description }}
       </li>
     </ul>
+    <div>
+      <input v-model="title" placeholder="title">
+      <input v-model="description" placeholder="description">
+      <button @click="addMemo">メモを追加</button>
+    </div>
   </div>
 </template>
 
@@ -14,7 +19,9 @@ import axios from 'axios';
 export default {
   data: function () {
     return {
-      memos: "memos"
+      memos: "memos",
+      title: '',
+      description: '',
     }
   },
   mounted () {
@@ -24,8 +31,17 @@ export default {
     setMemo: function () {
       axios.get('/api/memos')
       .then(response => (
-      this.memos = response.data
+        this.memos = response.data
       ))
+    },
+    addMemo: function() {
+      axios.post('/api/memos', {
+        title: this.title,
+        description: this.description
+      })
+      .then(response => (
+        this.setMemo()
+      ));
     }
   }
 }
